6a5df390ee30e34feb8b450fbe17eb13b62ea624
m0bilebtw <osrs.m0bile.btw@gmail.com>
2024-07-01 20:24:05 +0100

    Add private constructor for ProjectileSoundID

--- src/main/java/com/github/m0bilebtw/projectile/ProjectileSoundID.java
+++ src/main/java/com/github/m0bilebtw/projectile/ProjectileSoundID.java
@@ -3,5 +3,7 @@ package com.github.m0bilebtw.projectile;
 import java.util.Set;
 
 public class ProjectileSoundID {
+    private ProjectileSoundID() {}
+
     public static final Set<Integer> SNOWBALL_SOUND_IDS = Set.of(2708, 3292);
 }
